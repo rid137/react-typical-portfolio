929f00b6a0aede57513c6bde103414c53fcc7ed2
roycoMedia <makinderidwan73@gmail.com>
2023-03-26 16:48:12 +0100

    commit5

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,9 +14,9 @@ const Header = () => {
           <CTA />  
           <HeaderSocials /> 
 
-          {/* <div className="me">
+          <div className="me">
             <img src={ME} alt="" />
-          </div> */}
+          </div>
 
           <a href="#contact" className='scroll-down' >Scroll Down </a>
         </div>
