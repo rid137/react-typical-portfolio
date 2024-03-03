9bbf2ccf9e0edc22ff8f62c432ac8eb7ad38b5ca
roycoMedia <makinderidwan73@gmail.com>
2024-03-03 07:44:11 +0100

    new commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -61,7 +61,7 @@ const About = () => {
               <article className="about-card">
                 <FaAward className='about-icon' />
                 <h5>Experience</h5>
-                <small>1+ Years Working</small>
+                <small>3+ Years Working</small>
               </article>
 
               <article className="about-card">
